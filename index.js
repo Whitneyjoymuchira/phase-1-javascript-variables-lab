c697d5bc607a7520b46e6b6ec8a99af4911db389
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-15 23:14:44 +0300

    commit tree 1502733da24704177292627f24bb9af8f44dc5ca

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // Code your solution in this file
-const companyName='Scuber';
+const companyName = 'Scuber';
 
 let mostProfitableNeighborhood;
 mostProfitableNeighborhood = 'Chelsea';
 
 
 let companyCeo;
-companyCeo= 'Susan Smith'
+companyCeo = 'Susan Smith'
+
+
+
+
+
+
+function multiply(num1, num2) {
+    let result = num1 * num2
+    return result
+}
+
+multiply(20, 30)
+
+
+
